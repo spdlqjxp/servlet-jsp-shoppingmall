96cfe612cd6f36a917fdbce1a646d1767a04a400
bagjinhwan <spdlqjxp@gmail.com>
2025-04-24 15:49:36 +0900

    ADD LoginCheckFilter

--- src/main/java/com/nhnacademy/shoppingmall/common/filter/LoginCheckFilter.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/filter/LoginCheckFilter.java
@@ -10,10 +10,17 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
+@WebFilter(filterName = "loginCheckFilter", urlPatterns = "/mypage/*")
 public class LoginCheckFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         //todo#10 /mypage/ 하위경로의 접근은 로그인한 사용자만 접근할 수 있습니다.
-
+        String userId = (String) req.getSession().getAttribute("userId");
+        if (userId == null) {
+            log.debug("로그인 하지 않은 사용자입니다.");
+            res.sendRedirect("/login.do");
+        } else {
+            chain.doFilter(req, res);
+        }
     }
 }

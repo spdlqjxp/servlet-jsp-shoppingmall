e031484f4c0d508c800ef670bead500990da21fd
bagjinhwan <spdlqjxp@gmail.com>
2025-04-24 15:49:22 +0900

    ADD WelcomePageFilter

--- src/main/java/com/nhnacademy/shoppingmall/common/filter/WelcomePageFilter.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/filter/WelcomePageFilter.java
@@ -1,5 +1,7 @@
 package com.nhnacademy.shoppingmall.common.filter;
 
+import com.nhnacademy.shoppingmall.common.mvc.view.ViewResolver;
+import jakarta.servlet.annotation.WebServlet;
 import lombok.extern.slf4j.Slf4j;
 
 import jakarta.servlet.*;
@@ -11,12 +13,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
-
+@WebFilter(filterName = "welcomePageFilter", urlPatterns = "/")
 public class WelcomePageFilter extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         //todo#9 /요청이 오면 welcome page인 index.do redirect 합니다.
-
+        res.sendRedirect("/index.do");
     }
 }

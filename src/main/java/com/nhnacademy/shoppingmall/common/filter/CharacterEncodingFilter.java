d2c4b2ed212d7a094ae2c669335f300158063497
bagjinhwan <spdlqjxp@gmail.com>
2025-04-24 15:49:30 +0900

    ADD CharacterEncodingFilter

--- src/main/java/com/nhnacademy/shoppingmall/common/filter/CharacterEncodingFilter.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/filter/CharacterEncodingFilter.java
@@ -5,12 +5,25 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.annotation.WebInitParam;
 import java.io.IOException;
 
+
+@WebFilter(initParams = {@WebInitParam(name = "encoding",value = "UTF-8")})
 public class CharacterEncodingFilter  implements Filter {
+    private String encoding;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        encoding = filterConfig.getInitParameter("encoding");
+        if(encoding == null){
+            encoding = "UTF-8";
+        }
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         //todo#8 UTF-8 인코딩, initParams의 encoding parameter value값을 charset 으로 지정합니다.
         //@WebFilter(initParams = {@WebInitParam(name = "encoding",value = "UTF-8")})
+        servletRequest.setCharacterEncoding(encoding);
+        servletResponse.setCharacterEncoding(encoding);
+        filterChain.doFilter(servletRequest, servletResponse);
     }
-
 }

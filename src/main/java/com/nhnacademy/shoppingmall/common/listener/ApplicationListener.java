94f15b57d4af9547a84e9e28ea3184e33f81ecb8
bagjinhwan <spdlqjxp@gmail.com>
2025-04-24 16:11:26 +0900

    ADD Admin & User in ApplicationListener

--- src/main/java/com/nhnacademy/shoppingmall/common/listener/ApplicationListener.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/listener/ApplicationListener.java
@@ -1,19 +1,36 @@
 package com.nhnacademy.shoppingmall.common.listener;
 
+import com.nhnacademy.shoppingmall.user.domain.User;
+import com.nhnacademy.shoppingmall.user.repository.UserRepository;
 import com.nhnacademy.shoppingmall.user.repository.impl.UserRepositoryImpl;
 import com.nhnacademy.shoppingmall.user.service.UserService;
 import com.nhnacademy.shoppingmall.user.service.impl.UserServiceImpl;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.annotation.WebListener;
 import lombok.extern.slf4j.Slf4j;
 
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 
+import java.time.LocalDateTime;
+
 @Slf4j
+@WebListener
 public class ApplicationListener implements ServletContextListener {
     private final UserService userService = new UserServiceImpl(new UserRepositoryImpl());
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         //todo#12 application 시작시 테스트 계정인 admin,user 등록합니다. 만약 존재하면 등록하지 않습니다.
+        ServletContext context = sce.getServletContext();
+        User admin = new User("admin", "admin", "12345", "", User.Auth.ROLE_ADMIN, 1_000_000, null, null);
+        User user = new User("user", "user", "12345", "", User.Auth.ROLE_USER, 1_000_000, null, null);
+
+        userService.saveUser(admin);
+        userService.saveUser(user);
+
+        context.setAttribute("admin", admin);
+        context.setAttribute("user", user);
 
     }
 }

4db0d5980e4bbc1fd4a3441eb2a31758d25c41ba
bagjinhwan <spdlqjxp@gmail.com>
2025-04-24 10:30:19 +0900

    MYSQL initialize.

--- src/main/java/com/nhnacademy/shoppingmall/common/util/DbUtils.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/util/DbUtils.java
@@ -23,18 +23,23 @@ public class DbUtils {
         }
 
         //todo#1-1 {ip},{database},{username},{password} 설정
-
+        basicDataSource.setUrl("jdbc:mysql://10.116.64.14:13306/nhn_academy_16");
+        basicDataSource.setUsername("nhn_academy_16");
+        basicDataSource.setPassword("ANQDj5vXfM@1vTo@");
 
         //todo#1-2 initialSize, maxTotal, maxIdle, minIdle 은 모두 5로 설정합니다.
-
+        basicDataSource.setInitialSize(5);
+        basicDataSource.setMaxTotal(5);
+        basicDataSource.setMaxIdle(5);
+        basicDataSource.setMinIdle(5);
 
         //todo#1-3 Validation Query를 설정하세요
-
+        basicDataSource.setValidationQuery("SELECT 1");
 
         basicDataSource.setMaxWait(Duration.ofSeconds(2));
 
         //todo#1-4 적절히 변경하세요
-        DATASOURCE = null;
+        DATASOURCE = basicDataSource;
 
     }
 

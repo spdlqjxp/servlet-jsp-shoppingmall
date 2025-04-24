62d2a8900addd1ece6ad0ede220a9e9005c64cc8
bagjinhwan <spdlqjxp@gmail.com>
2025-04-24 10:30:32 +0900

    ADD Users Table.

---
+++ submit/erd/script.sql
@@ -0,0 +1,13 @@
+
+
+CREATE TABLE `users` (
+                         `user_id` varchar(50) NOT NULL COMMENT '아이디',
+                         `user_name` varchar(50) NOT NULL COMMENT '이름',
+                         `user_password` varchar(200) NOT NULL COMMENT 'mysql password 사용',
+                         `user_birth` varchar(8) NOT NULL COMMENT '생년월일 : 19840503',
+                         `user_auth` varchar(10) NOT NULL COMMENT '권한: ROLE_ADMIN,ROLE_USER',
+                         `user_point` int NOT NULL COMMENT 'default : 1000000',
+                         `created_at` datetime NOT NULL COMMENT '가입 일자',
+                         `latest_login_at` datetime DEFAULT NULL COMMENT '마지막 로그인 일자',
+                         PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='회원';

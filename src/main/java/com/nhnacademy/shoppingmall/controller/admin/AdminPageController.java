3cd2369c75b06db9e1e9076ce2e1c305d709f730
bagjinhwan <spdlqjxp@gmail.com>
2025-04-28 13:12:54 +0900

    ADD product view logic

---
+++ src/main/java/com/nhnacademy/shoppingmall/controller/admin/AdminPageController.java
@@ -0,0 +1,24 @@
+package com.nhnacademy.shoppingmall.controller.admin;
+
+import com.nhnacademy.shoppingmall.common.mvc.annotation.RequestMapping;
+import com.nhnacademy.shoppingmall.common.mvc.controller.BaseController;
+import com.nhnacademy.shoppingmall.product.repository.ProductRepository;
+import com.nhnacademy.shoppingmall.product.repository.impl.ProductCategoryRepositoryImpl;
+import com.nhnacademy.shoppingmall.product.repository.impl.ProductRepositoryImpl;
+import com.nhnacademy.shoppingmall.product.service.ProductService;
+import com.nhnacademy.shoppingmall.product.service.impl.ProductServiceImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequestMapping(method = RequestMapping.Method.GET, value = "/admin/product.do")
+public class AdminPageController implements BaseController {
+    private final ProductService productService = new ProductServiceImpl(new ProductRepositoryImpl(), new ProductCategoryRepositoryImpl());
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse resp) {
+        log.debug("Admin page requested");
+        req.setAttribute("productList", productService.getAllProducts());
+        return "shop/page/admin/admin_index";
+    }
+}

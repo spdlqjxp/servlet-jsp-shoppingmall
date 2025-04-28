a566b57ec8519a0ac8b7d083a4957824bfc5bf77
bagjinhwan <spdlqjxp@gmail.com>
2025-04-28 17:06:10 +0900

    ADD findCategoriesByProductId

--- src/main/java/com/nhnacademy/shoppingmall/product/repository/CategoryRepository.java
+++ src/main/java/com/nhnacademy/shoppingmall/product/repository/CategoryRepository.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface CategoryRepository {
     List<Category> findAll();
+
+    List<Category> findCategoriesByProductId(String productId);
 }
--- src/main/java/com/nhnacademy/shoppingmall/product/repository/impl/CategoryRepositoryImpl.java
+++ src/main/java/com/nhnacademy/shoppingmall/product/repository/impl/CategoryRepositoryImpl.java
@@ -17,19 +17,39 @@ public class CategoryRepositoryImpl implements CategoryRepository {
         String sql = """
                 select * from category
                 """;
-        try (Connection connection = DbConnectionThreadLocal.getConnection()) {
-            PreparedStatement statement = connection.prepareStatement(sql);
-            ResultSet rs = statement.executeQuery();
-            List<Category> categories = new ArrayList<>();
-            while (rs.next()) {
-                String categoryId = rs.getString("category_id");
-                String categoryName = rs.getString("category_name");
-                Category category = new Category(categoryId, categoryName);
-                categories.add(category);
-            }
-            return categories;
+        Connection connection = DbConnectionThreadLocal.getConnection();
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            return getCategories(statement);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public List<Category> findCategoriesByProductId(String productId) {
+        Connection connection = DbConnectionThreadLocal.getConnection();
+        String sql = """
+                select c.category_id, c.category_name from category_product cp
+                inner join category c on cp.category_id = c.category_id
+                where cp.product_id = ?
+                """;
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, productId);
+            return getCategories(statement);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<Category> getCategories(PreparedStatement statement) throws SQLException {
+        ResultSet rs = statement.executeQuery();
+        List<Category> categories = new ArrayList<>();
+        while (rs.next()) {
+            String categoryId = rs.getString("category_id");
+            String categoryName = rs.getString("category_name");
+            Category category = new Category(categoryId, categoryName);
+            categories.add(category);
+        }
+        return categories;
     }
 }

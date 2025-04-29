3703012a47e273cf0210a57f8a4cc7c9f463ccd9
bagjinhwan <spdlqjxp@gmail.com>
2025-04-29 18:00:08 +0900

    ADD User Delete Controller

---
+++ src/main/java/com/nhnacademy/shoppingmall/controller/user/UserDeleteController.java
@@ -0,0 +1,24 @@
+package com.nhnacademy.shoppingmall.controller.user;
+
+import com.nhnacademy.shoppingmall.common.mvc.annotation.RequestMapping;
+import com.nhnacademy.shoppingmall.common.mvc.controller.BaseController;
+import com.nhnacademy.shoppingmall.user.repository.impl.UserRepositoryImpl;
+import com.nhnacademy.shoppingmall.user.service.UserService;
+import com.nhnacademy.shoppingmall.user.service.impl.UserServiceImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequestMapping(method = RequestMapping.Method.POST, value = "/mypage/delete.do")
+public class UserDeleteController implements BaseController {
+    private final UserService userService = new UserServiceImpl(new UserRepositoryImpl());
+
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse resp) {
+        String userId = req.getParameter("user_id");
+        userService.deleteUser(userId);
+        log.debug("User with ID {} has been deleted.", userId);
+        return "redirect:/logout.do";
+    }
+}

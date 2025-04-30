3a48798d92ef793321a28d44ae4ceb1168b23934
bagjinhwan <spdlqjxp@gmail.com>
2025-04-30 14:26:41 +0900

    ADD cart.

--- src/main/java/com/nhnacademy/shoppingmall/controller/auth/LoginPostController.java
+++ src/main/java/com/nhnacademy/shoppingmall/controller/auth/LoginPostController.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.shoppingmall.controller.auth;
 
+import com.nhnacademy.shoppingmall.cart.Cart;
 import com.nhnacademy.shoppingmall.common.mvc.annotation.RequestMapping;
 import com.nhnacademy.shoppingmall.common.mvc.controller.BaseController;
 import com.nhnacademy.shoppingmall.user.domain.User;
@@ -12,6 +13,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RequestMapping(method = RequestMapping.Method.POST,value = "/loginAction.do")
 public class LoginPostController implements BaseController {
 
@@ -22,14 +26,15 @@ public class LoginPostController implements BaseController {
         //todo#13-2 로그인 구현, session은 60분동안 유지됩니다.
         String userId = req.getParameter("user_id");
         String password = req.getParameter("user_password");
+        List<Cart> cartList = new ArrayList<>();
         try {
             User user = userService.doLogin(userId, password);
             HttpSession session = req.getSession();
             session.setAttribute("user", user);
             session.setMaxInactiveInterval(60 * 60);//60분
-
+            session.setAttribute("cartList", cartList);
         } catch (UserNotFoundException e) {
-            return "redirect:/login.do";
+            return "redirect:/login.do?error=1";
         }
         return "redirect:/";
     }
